fcc18c16e33f17de72be4fc7045a4cfb005725cd
javatechie <basanta.kumar.hota.4@gmail.com>
2024-02-25 16:25:10 +0530

    name optimized

--- src/main/java/com/javatechie/JenkinsCiCdApplication.java
+++ src/main/java/com/javatechie/JenkinsCiCdApplication.java
@@ -13,6 +13,9 @@ public class JenkinsCiCdApplication {
 
     @GetMapping("/greetings/{name}")
     public String greetings(@PathVariable String name) {
+        //Basant kumar hota
+        //sarvindra kumar
+       name= name.split(" ")[0];
         return "Hello " + name + " Congratulations you have successfully completed Jenkins CI/CD demo !";
     }
 
